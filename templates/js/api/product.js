4d44252eb0de3b40d410e7e261a8f25e4107a3b7
Matheus Reis <matheus.reis@olist.com>
2020-08-20 11:41:26 -0300

    product table loading data

---
+++ templates/js/api/product.js
@@ -0,0 +1,35 @@
+var product_endpoint = 'http://localhost:5000/api/product/'
+
+function get_all() {
+    $.ajax({
+        type : 'GET',
+        url : product_endpoint,
+        dataType : 'json',
+        success: (data) => {
+            load_table(data);
+        },
+        error: (e) => {
+            console.log('api erro')
+        }
+    });
+}
+
+function load_table(data) {
+    data.forEach((item) => {
+        data += `<tr>
+            <td width="15%">${item['name']}</td>"
+            <td>${item['description']}</td>"
+            <td width="5%">${item['price']}</td>"
+            <td width="10%">${item['status']}</td>"
+            <td width="10%">${item['approved']}</td>"
+            <td width="15%" data-id="${item['id']}">
+                <a class='btn-edit' href='#'>Editar</a> |
+                <a class='btn-delete' href='#'>Deletar</a>
+            </td>
+        </tr>`;
+    });
+
+    $('table tbody').html(data);
+}
+
+$(document).ready(() => get_all());
